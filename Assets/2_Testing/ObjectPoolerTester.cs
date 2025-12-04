fad10d96b46e75f8c0a20bceeb119ede56a6eb7c
Daniel-PC <danielnoam1999@gmail.com>
2025-12-04 21:00:04 +0200

    Update

---
+++ Assets/2_Testing/ObjectPoolerTester.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using DNExtensions.Button;
+using DNExtensions.ObjectPooling;
+using UnityEngine;
+
+public class ObjectPoolerTester : MonoBehaviour
+{
+    [SerializeField] private float bulletLifeTime = 5f;
+    [SerializeField] private float rateOfFire = 1;
+    [SerializeField] private float bulletSpeed = 10f;
+    [SerializeField] private GameObject bulletPrefab;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnBulletsRoutine());
+    }
+
+    private IEnumerator SpawnBulletsRoutine()
+    {
+        while (true)
+        {
+            SpawnBullet();
+            yield return new WaitForSeconds(1f / rateOfFire);
+        }
+    }
+
+    private void SpawnBullet()
+    {
+        var bulletGo = ObjectPooler.GetObjectFromPool(bulletPrefab);
+        StartCoroutine(BulletLifetimeRoutine(bulletGo));
+    }
+
+    private IEnumerator BulletLifetimeRoutine(GameObject bullet)
+    {
+        float elapsedTime = 0f;
+        
+        while (elapsedTime < bulletLifeTime)
+        {
+            bullet.transform.position += bullet.transform.forward * (bulletSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        ObjectPooler.ReturnObjectToPool(bullet);
+    }
+}
